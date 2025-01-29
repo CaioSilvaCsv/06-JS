c6cb839607203e1b1c148112166fe9dcd6903d0a
Caio da Silva <caiosil.viana@gmail.com>
2025-01-29 00:28:51 -0300

    Alteração total do modo contra o computador

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 //quadro do jogo
 let jogo;
-let jogoWidth = 500;
+let jogoWidth = 375;
 let jogoHeight = 500;
 let context;
+
+let tamanhoNavegador = window.innerWidth;
+if (tamanhoNavegador >800) jogoWidth = 500;
 
 //jogador
 let jogadorWidth = 80;
 let jogadorHeight = 10;
-let jogadorSpeedX =  10
+let jogadorSpeedX =  10;
 
 let nomeJogador = "";
 
@@ -22,15 +25,17 @@ let jogador = {
 let pontuacao = 0;
 let gameOver = false;
 
+let velocidadeBolaX = 3;
+let velocidadeBolaY = 2; 
 
 //bola
 let bolaWidth = 10;
 let bolaHeight = 10;
-let bolaVelocityX = 3;
-let bolaVelocityY = 2;
+let bolaVelocityX = velocidadeBolaX;
+let bolaVelocityY = velocidadeBolaY;
 
 let bola = {
-    x : numAleatorio(0, 500),
+    x : numAleatorio(0, jogoWidth-10),
     y : jogoHeight/2,
     width : bolaWidth,
     height : bolaHeight,
@@ -42,7 +47,8 @@ let bola = {
 let blockArray = [];
 let blockWidth = 50;
 let blockHeight = 10;
-let blockColumns = 8;
+let blockColumns = 6;
+if (jogoWidth == 500) blockColumns = 8;
 let blockRows = 3; // É adicionado mais ao decorrer do jogo.
 let blockMaxRows = 10;
 let blockCount = 0;
@@ -104,16 +110,19 @@ function update(){
             , jogadorComputador.width, jogadorComputador.height);
     
         let jogadorComputadorCentro = jogadorComputador.x + jogadorComputador.width / 2;
-        if (bola.x > jogadorComputadorCentro) {
-            let nextJogadorX = jogadorComputador.x + jogadorComputadorSpeedX;
-            if (!limites({ x: nextJogadorX, width: jogadorComputadorWidth })) {
-                jogadorComputador.x = nextJogadorX;
+        console.log(bolaComputador.velocidadeBolaY)
+        if (bolaComputador.y > (jogoHeight / 2) + 10 && bolaComputador.velocityY > 0) {
+                if (bolaComputador.x > jogadorComputadorCentro) {
+                let nextJogadorX = jogadorComputador.x + jogadorComputadorSpeedX;
+                if (!limites({ x: nextJogadorX, width: jogadorComputadorWidth })) {
+                    jogadorComputador.x = nextJogadorX;
+                }
             }
-        }
-         else if (bola.x < jogadorComputadorCentro) {
-            let nextJogadorX = jogadorComputador.x - jogadorComputadorSpeedX;
-            if (!limites({ x: nextJogadorX, width: jogadorComputadorWidth })) {
-                jogadorComputador.x = nextJogadorX;
+            else if (bolaComputador.x < jogadorComputadorCentro) {
+                let nextJogadorX = jogadorComputador.x - jogadorComputadorSpeedX;
+                if (!limites({ x: nextJogadorX, width: jogadorComputadorWidth })) {
+                    jogadorComputador.x = nextJogadorX;
+                }
             }
         }
     }
@@ -124,37 +133,34 @@ function update(){
     bola.y += bola.velocityY;
     context.fillRect(bola.x, bola.y, bola.width, bola.height);
 
-    //limite da bola na parede
-    if(bola.y <= 0){
-        //se a bola tocar no topo do canvas.
-        bola.velocityY *= -1;
-
-    }else if(bola.x <= 0 || (bola.x + bola.width)>= jogoWidth){
-        //Se a bola tocar na borda lateral do canva.
-        bola.velocityX *= -1;
-    }else if(bola.y + bola.height>= jogoHeight){
-        //Se a bola tocar embaixo do canva
-        //Fim de jogo
-        if(modoComputador){
-            pontuacaoComputador++;
-            bola.velocityY*= -1;
-            if(pontuacaoComputador == 4){
-                context.font = "15px sans-serif";
-                context.fillText(" O computador ganhou!", 180, 400);
-                fimJogo();
-            }
-        }else{
-            context.font = "15px sans-serif";
-            fimJogo();
-        }        
+    //Bola computador
+    if(modoComputador){
+        context.fillStyle = "yellow";
+        bolaComputador.x += bolaComputador.velocityX;
+        bolaComputador.y += bolaComputador.velocityY;
+        context.fillRect(bolaComputador.x, bolaComputador.y, bolaComputador.width, bolaComputador.height);
     }
+
+    //A detecção da bola batendo nas paredes e o rebote e caso toque no "chão" será descontado 100 pontos.
+    if (fim = rebote(bola) == 1) pontuacao - 100;
+    if (fim = rebote(bolaComputador) == 1 ) pontuacaoComputador - 100;
 
     // bater a bola e subir novamente.
     if(topColisao(bola, jogador) || bottomColisao(bola, jogador)){
         bola.velocityY *= -1; //Virar y, seja pra cima ou pra baixo.
 
-    }else if(leftColisao(bola, jogador)){
+    }else if(leftColisao(bola, jogador) || rightColisao(bola, jogador)){
         bola.velocityX *= -1; // Virar x
+    }
+
+    // bater a bola e subir novamente no modo computador.
+    if(modoComputador){
+        if(topColisao(bolaComputador, jogadorComputador) || bottomColisao(bolaComputador, jogadorComputador)){
+            bolaComputador.velocityY *= -1; //Virar y, seja pra cima ou pra baixo.
+            
+        }else if(leftColisao(bolaComputador, jogadorComputador) || rightColisao(bolaComputador, jogadorComputador)){
+            bolaComputador.velocityX *= -1; // Virar x
+        }
     }
 
     //blocos
@@ -173,13 +179,28 @@ function update(){
                 blockCount -= 1;
                 pontuacao += 100;
             }
-            context.fillRect(block.x, block.y, block.width, block.height);
+            else if(topColisao(bolaComputador, block) || bottomColisao(bolaComputador, block)){
+                block.break = true;
+                bolaComputador.velocityY *= -1; //Mudar a direção de y, para cima ou para baixo.
+                blockCount -= 1;
+                pontuacaoComputador += 100;
+            }else if(leftColisao(bolaComputador, block) || rightColisao(bolaComputador, block)){
+                block.break = true;
+                bolaComputador.velocityX *= -1; //mudar a direção de x, direita ou esquerda.
+                blockCount -= 1;
+                pontuacaoComputador += 100;
+            }
+        
+           context.fillRect(block.x, block.y, block.width, block.height);
         }
     }
 
     //quando acabar os blocos.
     if (blockCount == 0){
         pontuacao += 100*blockRows*blockColumns; //Pontuação extra
+        pontuacaoComputador += 100*blockRows*blockColumns;
+        velocidadeBolaX += 2;//Quando os blocos acabarem a velocidade irá aumentar.
+        velocidadeBolaY += 2;
         blockRows = Math.min(blockRows + 1, blockMaxRows);
         createBlocks();
     }
@@ -228,6 +249,24 @@ function rightColisao(bola, block){     // a a direita de b (bola quando toca na
     return detectorColisao(bola, block) && (block.x + block.width) <= bola.x;
 }
 
+//limite da bola na parede
+function rebote(bola){
+        
+    if(bola.y <= 0){
+        //se a bola tocar no topo do canvas.
+        bola.velocityY *= -1;
+        
+    }else if(bola.x <= 0 || (bola.x + bola.width)>= jogoWidth){
+        //Se a bola tocar na borda lateral do canva.
+        bola.velocityX *= -1;
+    }else if(bola.y + bola.height>= jogoHeight){
+        //Se a bola tocar embaixo do canva
+        //Fim de jogo
+        fimJogo();
+        return 1;     
+    }
+}
+
 function createBlocks(){
     blockArray = [];
     for (let c = 0; c < blockColumns; c++){
@@ -249,6 +288,9 @@ function resetGame(){
     nomeJogador = jogadorNome();
     gameOver = false;
 
+    velocidadeBolaX = 3;    
+    velocidadeBolaY = 2; 
+
     jogador = {
         x: jogoWidth/2 - jogadorWidth/2,
         y:  jogoHeight - jogadorHeight - 5,
@@ -259,20 +301,29 @@ function resetGame(){
 
     jogadorComputador = {
         x: jogoWidth / 2 - jogadorComputadorWidth / 2,
-        y: 5,
+        y: jogoHeight - jogadorHeight - 5,
         width: jogadorComputadorWidth,
         height: jogadorComputadorHeight,
         velocityX: jogadorComputadorSpeedX
     };
 
     bola = {
-        x : numAleatorio(0, 500),
+        x : numAleatorio(0, jogoWidth-10),
         y : jogoHeight/2,
         width : bolaWidth,
         height : bolaHeight,
         velocityX : bolaVelocityX,
         velocityY : bolaVelocityY
-    };
+    }
+
+    bolaComputador = {
+        x : numAleatorio(0, jogoWidth-10),
+        y : jogoHeight/2,
+        width : bolaWidth,
+        height : bolaHeight,
+        velocityX : bolaCVelocityX,
+        velocityY : bolaCVelocityY
+    }
 
     blockArray = [];
     blockRows = 3;
@@ -327,12 +378,27 @@ function salvaPontuacao(jogadorNome, pontuacao){
 }
 
 function fimJogo(){
-    context.fillText("Fim de jogo: Precione 'Espaço' para reiniciar.", 100, 420);
+    context.fillStyle = "white"
+    frase("sans-serif","Fim de jogo: Precione 'Espaço' para reiniciar.", 420);
     context.font="25px monospace";
-    context.fillText(`${nomeJogador} adiquiriu ${pontuacao} pontos.`, 50, 200);
+    frase("25px monospace",`${nomeJogador} adiquiriu ${pontuacao} pontos.`, 200);
+    if(modoComputador) ganhador(pontuacao, pontuacaoComputador);
     salvaPontuacao(nomeJogador, pontuacao);
     listarRanking();
     gameOver = true;
+}
+
+//para definir no meio da tela a frase
+function frase(font, frase, posicao){
+    context.font = font;
+    let largura = (jogoWidth-context.measureText(frase).width)/2;
+    context.fillText(frase, largura, posicao);
+}
+
+//Faz a comparação com as pontuações e verifica quem ganhou 
+function ganhador(pt, ptC){
+    let ganhador = pt == ptC ?  " Houve um empate " : pt > ptC ? "Você ganhou" :  "O computador ganhou";
+    frase("15px monospace", `${ganhador}`, 400);
 }
 
 
@@ -341,14 +407,27 @@ let modoComputador = false;
 
 let jogadorComputadorWidth = 80;
 let jogadorComputadorHeight = 10;
-let jogadorComputadorSpeedX = 10;
+let jogadorComputadorSpeedX = 5;//Velocidade de movimento do jogador.
 let pontuacaoComputador = 0;
 let pontuacaoJogador = 0;
 
 let jogadorComputador = {
     x: jogoWidth / 2 - jogadorComputadorWidth / 2,
-    y: 5,
+    y: jogoHeight - jogadorHeight - 5,
     width: jogadorComputadorWidth,
     height: jogadorComputadorHeight,
     velocityX: jogadorComputadorSpeedX
 };
+
+//Bola do computador
+let bolaCVelocityX = velocidadeBolaX;
+let bolaCVelocityY = velocidadeBolaX;
+
+let bolaComputador = {
+    x : numAleatorio(0, jogoWidth-10),
+    y : jogoHeight/2,
+    width : bolaWidth,
+    height : bolaHeight,
+    velocityX : bolaCVelocityX,
+    velocityY : bolaCVelocityY
+}
